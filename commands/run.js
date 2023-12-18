8ca7c012cc4ae40d8f0a38432f94b8d848019609
hkartt <humzakhan7678@gmail.com>
2023-12-18 18:13:19 -0500

    fixed bugs

--- commands/run.js
+++ commands/run.js
@@ -32,23 +32,25 @@ module.exports = {
                     // }
 
                     setTimeout(function() {
-                        city = obj[interaction.guildId].city;
-                        country = obj[interaction.guildId].country;
-                        fn.addTime(calledAt, lastTime).then((currentTime) => {
+                        let city = obj[interaction.guildId].city;
+                        let country = obj[interaction.guildId].country;
+                        let oldTime = calledAt;
+                        let milliseconds = lastTime;
+                        fn.addTime(oldTime, milliseconds).then((currentTime) => {
                             fn.nextPrayer(currentTime, city, country).then((obj) => {
                                 fn.timeDifference(Object.values(obj)[0], currentTime).then((time) => {
                                     console.log("ran callback command at " + currentTime);
                                     interaction.channel.send("@everyone it is now " + lastPrayer + " time and the next prayer is " + Object.keys(obj)[0] + " at "  + Object.values(obj)[0]);
-                                    prayerTimeout(time, Object.keys(obj)[0]);
+                                    prayerTimeout(time, Object.keys(obj)[0], currentTime);
                                 });
                             });
                         });
                         
                     }, lastTime);
                 }
-                city = obj[interaction.guildId].city;
-                country = obj[interaction.guildId].country;
-                currentTime = interaction.createdAt.getHours() + ":" + interaction.createdAt.getMinutes();
+                let city = obj[interaction.guildId].city;
+                let country = obj[interaction.guildId].country;
+                let currentTime = interaction.createdAt.getHours() + ":" + interaction.createdAt.getMinutes();
                 fn.nextPrayer(currentTime, city, country).then((obj) => {
                     fn.timeDifference(Object.values(obj)[0], currentTime).then((time) => {
                         interaction.channel.send("The next prayer is " + Object.keys(obj)[0] + " at "  + Object.values(obj)[0]);
--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
     once: true,
     async execute(client) {
         console.log(`logged in as ${client.user.tag}`);
-        
+        // for each guild, check if they have notification setting toggled. implement later
     }
 }
